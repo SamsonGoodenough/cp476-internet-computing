892c82ae382b021bb012f3913192ca3fa741c179
Samson Goodenough <goodenough.samson@gmail.com>
2023-03-10 21:26:34 -0500

    Update envs

--- php/src/index.php
+++ php/src/index.php
@@ -8,9 +8,9 @@ $user = getenv('MYSQL_USER');
 $pass = getenv('MYSQL_PASSWORD');
 
 // check the MySQL connection status
-$conn = new mysqli($host, $user, $pass);
+$conn = new mysqli($host, $user, $pass, 'test');
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } else {
-    echo "Connected to MySQL server successfully!";
+    print("Connected to MySQL server successfully!\n");
 }
